ff4ad69a214386489f1f9d89777cee95df6b4de0
minji4757 <minji4757@naver.com>
2018-11-07 12:39:06 +0900

    모든 필드를 삭제했다.

--- src/Test1.java
+++ src/Test1.java
@@ -1,13 +1,3 @@
 public class Test1 {
-    private int f1;
-    private int f2;
-    private int f3;
-    private int f4;
-    private int f5;
-    private int f6;
-    private int f7;
-    private int f9;
-    private int f8;
-    private int f10;
-    private int f11;
+
 }
